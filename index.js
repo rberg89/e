a96b143f83f8064e299b734b7e00ad9bd68d7070
Ryan <rberg89@gmail.com>
2021-12-17 22:32:17 -0600

    added documentation

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const EXEC_LIMIT = 10000000;
 
+// create array of test cases of this function
+var y = [];
+for (var i=0; i<EXEC_LIMIT; i++) {
+  y.push(e(Math.random(), Math.random(), 1));
+}
+
+// average the test cases
+var sum = 0;
+for (var i=0; i<EXEC_LIMIT; i++) {
+  sum += y[i];
+}
+ave = sum / EXEC_LIMIT;
+
+// output / done
+console.log(ave);
+
+
+/** 
+ * Computes 1 iteration using JS Math.random()
+ * @param {Number} x: the sum of random numbers in this iteration
+ * @param {Number} y: the random number to add to to x
+ * @param {Number} i: the number of iterations
+ * 
+ * @return {Number} i: the number of iterations
+*/
+
 function e(x, y, i) {
   i++;
   x += y;
@@ -9,14 +35,3 @@ function e(x, y, i) {
     return i;
   }
 }
-var y = [];
-for (var i=0; i<EXEC_LIMIT; i++) {
-  y.push(e(Math.random(), Math.random(), 1));
-}
-var sum = 0;
-for (var i=0; i<EXEC_LIMIT; i++) {
-  sum += y[i];
-}
-ave = sum / EXEC_LIMIT;
-
-console.log(ave);
